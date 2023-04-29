14cf811636c90950d9ca1f43d79a358b2732aff0
aleksandar.acko.jovanovic@gmail.com <aleksandar.acko.jovanovic@gmail.com>
2023-04-29 19:40:18 +0200

    dynamic navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,42 @@
+const BASE_URL= "https://gringus0.github.io/savoryspot/";
+var url = document.location.pathname;
+console.log(url);
 window.onload = function(){
-
+    ajaxCB("menu.json", function(result){
+        navigation(result);
+    });
+    
 }
 
+function navigation(array){
+    let header = document.querySelector("header");
+    let html = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#" id="nav-title">SavorySpot</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav ms-auto">`;
+        for(let item of array){
+            html += `<li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="${item.href}">${item.name}</a>
+                    </li>`;
+        }
+                    html += `</ul>
+                            <form class="d-flex">
+                                <input class="form-control me-2" type="search" placeholder="Search for recipes" aria-label="Search">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>`
+    header.innerHTML = html;
+}
 
 function ajaxCB(file, result){
     $.ajax({
-        url: "assets/data" + file,
+        url: "assets/data/json/" + file,
         method: "get",
         dataType: "json",
         success: result,
@@ -20,16 +51,20 @@ function ajaxCB(file, result){
             }
             else if (xhr.status == 500) {
                 msg = 'Internal Server Error [500].';
-            } else if (exception === 'parsererror') {
+            } 
+            else if (exception === 'parsererror') {
                 msg = 'Requested JSON parse failed.';
-            } else if (exception === 'timeout') {
+            } 
+            else if (exception === 'timeout') {
                 msg = 'Timeout error.';
-            } else if (exception === 'abort') {
+            } 
+            else if (exception === 'abort') {
                 msg = 'Ajax request aborted.';
-            } else {
+            } 
+            else {
                 msg = 'Uncaught Error.\n' + xhr.responseText;
             }
-            alert(msg);
+            
         }
     })
 }
