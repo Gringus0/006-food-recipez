65b8a96a5cb6f8357fbfc2e3ccd97176cd1bfe13
aleksandar.acko.jovanovic@gmail.com <aleksandar.acko.jovanovic@gmail.com>
2023-04-30 17:54:08 +0200

    category filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const BASE_URL= "https://gringus0.github.io/savoryspot/";
 const BASE_IMG = "assets/img/";
 var url = document.location.pathname;
 console.log(url);
+let categories =  getFromLS("");
 window.onload = function(){
     ajaxCB("menu.json", function(result){
         navigation(result);
@@ -19,16 +20,29 @@ window.onload = function(){
         createRadio("Prep + Cook time: ", "sortTime", ["sort-time-asc", "sort-time-desc"], ["Ascending", "Descending"])
         ajaxCB("categories.json", function(result){
             result.forEach(category => {
-                createCheckbox(category.name);
+                createCheckbox(category.name, category.id);
+            });
+            addToLS("categoriesJSON", result);
+            const categoryCheckboxes = document.querySelectorAll('.category');
+            categoryCheckboxes.forEach(function(checkbox) {
+                checkbox.addEventListener('change', filterChange);
             });
         })
+        
     }
 }
 
+function addToLS(name, data){
+    localStorage.setItem(name, JSON.stringify(data));
+}
 
-function createCheckbox(name){
+function getFromLS(name){
+    return JSON.parse(localStorage.getItem(name));
+}
+
+function createCheckbox(name, id){
     let html = `<div class="form-check">
-                <input class="form-check-input" type="checkbox" value="${name.toLowerCase()}" id="${name}" name="${name.toLowerCase()}">
+                <input class="form-check-input category" type="checkbox" value="${id}" id="${name}" name="${name.toLowerCase()}">
                 <label class="form-check-label" for="${name}">${name}</label>
             </div>`        
     document.querySelector("#filter").innerHTML += html;
@@ -44,17 +58,45 @@ function createRadio(nameOfList, name, idArray, labelArray){
                             </div>`         
             }
     document.querySelector("#sort").innerHTML += html;
-            
+}
+
+function categoryFilter(array) {
+    let checkedCategories = [];
+    const categoryCheckboxes = document.querySelectorAll('.category:checked');
+    categoryCheckboxes.forEach(function(checkbox) {
+      checkedCategories.push(parseInt(checkbox.value));
+    });
+    if (checkedCategories.length !== 0) {
+      return array.filter(function(a) {
+        return a.categoryId.some(function(b) {
+          return checkedCategories.includes(b);
+        });
+      });
+    }
+    // console.log(array);
+    return array;
 }
 
 function writeCardList(array){
     let html = ``;
+    array = categoryFilter(array);
+    // console.log(array);
     for(let item of array){
+        let categoriesJSON = getFromLS("categoriesJSON");
+        let categories = [];
         html += `<div class="card col-4 mx-3" style="width: 18rem;">
                     <img src="${BASE_IMG}${item.img.src}" class="card-img-top" alt="${item.img.alt}"/>
                     <div class="card-body">`
-                    
+                    categoriesJSON.forEach(category => {
+                        item.categoryId.forEach(itemCategoryId => {
+                            if(category.id == itemCategoryId){
+                                categories.push(category.name);
+                            }
+                        });
+                    })
+                    let categoryText = categories.join(", ");
                     html += `<h5 class="card-title">${item.title}</h5>
+                        <p class="card-text">${categoryText}</p>
                         <p class="card-text">Prep time: ${item.prep_time} minutes</p>
                         <p class="card-text">Cook time: ${item.cook_time} minutes</p>
                         <p class="card-text">${item.description}</p>
@@ -96,6 +138,12 @@ function footer(){
                 <a href="#" class="text-dark">Aleksandar Jovanović 104/21</a>
                 <p class="m-0">&copy Visoka ICT Škola</p>`
     document.querySelector("footer").innerHTML = html;
+}
+
+function filterChange(){
+    ajaxCB("recipes.json", function(result){
+        writeCardList(result);
+    })
 }
 
 function ajaxCB(file, result){
